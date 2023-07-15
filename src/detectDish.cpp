92b18aa3e4bbd9ae37493621166186106d93aaef
AJ <arjunjassal09@gmail.com>
2023-07-15 15:43:49 +0200

    fix

--- src/detectDish.cpp
+++ src/detectDish.cpp
@@ -2,11 +2,9 @@
 // Created by Gabriel on 10/06/23.
 //
 
-
 #include "detectDish.h"
 using namespace std;
 using namespace cv;
-
 
 
 Mat detectDishesEdge(Mat image)
@@ -26,7 +24,7 @@ Mat detectDishesEdge(Mat image)
     {
         Vec3i c = circles[i];
         Point center = Point(c[0],c[1]);
-       // circle(ImageCircles, center, 1,Scalar(0,100,100),3,LINE_AA);
+        //circle(ImageCircles, center, 1,Scalar(0,100,100),3,LINE_AA);
         int radius = c[2];
         //circle(ImageCircles,center,radius,Scalar(0,0,0),3,LINE_AA);
         circle(mask, center, radius, Scalar(255), -1);
@@ -49,67 +47,66 @@ Mat watershedByOpencCV(Mat src)
     Mat mask;
     inRange(src, Scalar(255, 255, 255), Scalar(255, 255, 255), mask);
     src.setTo(Scalar(0, 0, 0), mask);
-// Show output image
-//imshow("Black Background Image", src);
-// Create a kernel that we will use to sharpen our image
-    Mat kernel = (Mat_<float>(3, 3) <<
-                                    1, 1, 1,
-            1, -8, 1,
-            1, 1, 1); // an approximation of second derivative, a quite strong kernel
-// do the laplacian filtering as it is
-// well, we need to convert everything in something more deeper then CV_8U
-// because the kernel has some negative values,
-// and we can expect in general to have a Laplacian image with negative values
-// BUT a 8bits unsigned int (the one we are working with) can contain values from 0 to 255
-// so the possible negative number will be truncated
+    // Show output image
+    //imshow("Black Background Image", src);
+    // Create a kernel that we will use to sharpen our image
+    Mat kernel = (Mat_<float>(3, 3) <<1, 1, 1,
+                                      1, -8, 1,
+                                      1, 1, 1); // an approximation of second derivative, a quite strong kernel
+    // do the laplacian filtering as it is
+    // well, we need to convert everything in something more deeper then CV_8U
+    // because the kernel has some negative values,
+    // and we can expect in general to have a Laplacian image with negative values
+    // BUT a 8bits unsigned int (the one we are working with) can contain values from 0 to 255
+    // so the possible negative number will be truncated
     Mat imgLaplacian;
     filter2D(src, imgLaplacian, CV_32F, kernel);
     Mat sharp;
     src.convertTo(sharp, CV_32F);
     Mat imgResult = sharp - imgLaplacian;
-// convert back to 8bits gray scale
+    // convert back to 8bits gray scale
     imgResult.convertTo(imgResult, CV_8UC3);
     imgLaplacian.convertTo(imgLaplacian, CV_8UC3);
-// imshow( "Laplace Filtered Image", imgLaplacian );
-//imshow( "New Sharped Image", imgResult );
-// Create binary image from source image
+    // imshow( "Laplace Filtered Image", imgLaplacian );
+    // imshow( "New Sharped Image", imgResult );
+    // Create binary image from source image
     Mat bw;
     cvtColor(imgResult, bw, COLOR_BGR2GRAY);
     threshold(bw, bw, 40, 255, THRESH_BINARY | THRESH_OTSU);
-//imshow("Binary Image", bw);
-// Perform the distance transform algorithm
+    // imshow("Binary Image", bw);
+    // Perform the distance transform algorithm
     Mat dist;
     distanceTransform(bw, dist, DIST_L2, 3);
-// Normalize the distance image for range = {0.0, 1.0}
-// so we can visualize and threshold it
+    // Normalize the distance image for range = {0.0, 1.0}
+    // so we can visualize and threshold it
     normalize(dist, dist, 0, 1.0, NORM_MINMAX);
-//imshow("Distance Transform Image", dist);
-// Threshold to obtain the peaks
-// This will be the markers for the foreground objects
+    // imshow("Distance Transform Image", dist);
+    // Threshold to obtain the peaks
+    // This will be the markers for the foreground objects
     threshold(dist, dist, 0.4, 1.0, THRESH_BINARY);
-// Dilate a bit the dist image
+    // Dilate a bit the dist image
     Mat kernel1 = Mat::ones(3, 3, CV_8U);
     dilate(dist, dist, kernel1);
-//imshow("Peaks", dist);
-// Create the CV_8U version of the distance image
-// It is needed for findContours()
+    // imshow("Peaks", dist);
+    // Create the CV_8U version of the distance image
+    // It is needed for findContours()
     Mat dist_8u;
     dist.convertTo(dist_8u, CV_8U);
-// Find total markers
+    // Find total markers
     vector<vector<Point> > contours;
     findContours(dist_8u, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-// Create the marker image for the watershed algorithm
+    // Create the marker image for the watershed algorithm
     Mat markers = Mat::zeros(dist.size(), CV_32S);
-// Draw the foreground markers
+    // Draw the foreground markers
     for (size_t i = 0; i < contours.size(); i++) {
         drawContours(markers, contours, static_cast<int>(i), Scalar(static_cast<int>(i) + 1), -1);
     }
-// Draw the background marker
+    // Draw the background marker
     circle(markers, Point(5, 5), 3, Scalar(255), -1);
     Mat markers8u;
     markers.convertTo(markers8u, CV_8U, 10);
-//imshow("Markers", markers8u);
-// Perform the watershed algorithm
+    //imshow("Markers", markers8u);
+    // Perform the watershed algorithm
     watershed(imgResult, markers);
     Mat mark;
     markers.convertTo(mark, CV_8U);
@@ -122,9 +119,9 @@ Mat watershedByOpencCV(Mat src)
         int r = theRNG().uniform(0, 256);
         colors.push_back(Vec3b((uchar) b, (uchar) g, (uchar) r));
     }
-// Create the result image
+    // Create the result image
     Mat dst = Mat::zeros(markers.size(), CV_8UC3);
-// Fill labeled objects with random colors
+    // Fill labeled objects with random colors
     for (int i = 0; i < markers.rows; i++) {
         for (int j = 0; j < markers.cols; j++) {
             int index = markers.at<int>(i, j);
@@ -303,7 +300,7 @@ Mat removeDishes(Mat image, int delta)
             {
                 img.at<Vec3b>(i,j) [0]= 0;
                 img.at<Vec3b>(i,j) [1]= 0;
-                img.at<Vec3b>(i,j) [2] = 0;
+                img.at<Vec3b>(i,j) [2]= 0;
             }
         }
     }
